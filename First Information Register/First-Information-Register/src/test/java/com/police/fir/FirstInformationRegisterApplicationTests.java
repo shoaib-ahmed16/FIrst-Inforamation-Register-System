5f707c44981c75a6bb664a893c11717d0b0e2edb
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2023-03-19 17:29:48 +0530

    adding fir system spring boot project structure

---
+++ First Information Register/First-Information-Register/src/test/java/com/police/fir/FirstInformationRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.police.fir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstInformationRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
