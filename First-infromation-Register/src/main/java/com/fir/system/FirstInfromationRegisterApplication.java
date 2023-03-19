eb512e40588635041289ba225c220fdc71569abc
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2023-03-19 17:43:18 +0530

    Added the files

---
+++ First-infromation-Register/src/main/java/com/fir/system/FirstInfromationRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.fir.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInfromationRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstInfromationRegisterApplication.class, args);
+	}
+
+}
---
+++ First-infromation-Register/src/test/java/com/fir/system/FirstInfromationRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fir.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstInfromationRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
